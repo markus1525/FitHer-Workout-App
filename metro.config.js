25d95f8b441a32bb5f9f455f58f26281a50d8914
Min Thu Kyaw Khaung <204016955+markus1525@users.noreply.github.com>
2026-06-09 21:42:04 +0800

    fix: add mov to metro asset extensions

--- metro.config.js
+++ metro.config.js
@@ -3,6 +3,9 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+// Allow .mov/.MOV video files to be bundled as static assets
+config.resolver.assetExts.push("mov");
+
 module.exports = withNativeWind(config, {
   input: "./global.css",
   // Force write CSS to file system instead of virtual modules
